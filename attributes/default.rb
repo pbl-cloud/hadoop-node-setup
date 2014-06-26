a5ac564880046c636067f627d0ecb92135c8db0d
Daniel Perez <tuvistavie@gmail.com>
2014-06-26 14:54:48 +0900

    Add notebook ssh key.

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,5 +3,5 @@ default['java']['jdk_version'] = 7
 default['hadoop']['hadoop_env']['JAVA_HOME'] = '/usr/lib/jvm/java-7-openjdk-amd64'
 
 default['ssh_keys'] = {
-  'vagrant' => ['daniel']
+  'vagrant' => ['daniel', 'daniel_vaio']
 }
